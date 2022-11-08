f9f4163d962cc49ca30f6af662d57044eae57268
claraiscoding <clara.brusamolino@gmail.com>
2022-11-08 15:54:27 +0100

    Cose

---
+++ src/main/java/com/survey/model/Category.java
@@ -0,0 +1,37 @@
+package com.survey.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Category () { }
+
+    public Category (String name) {
+        this.name = name;
+    }
+
+    public long getId () {
+        return this.id;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + "]";
+    }
+}
